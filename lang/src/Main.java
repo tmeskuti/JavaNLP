4389dffb9e7002649428298ae65fc58203c70980
teomeskuti <teomeskuti29@gmail.com>
2023-01-01 18:39:33 +0100

    Fix my mess

--- lang/src/Main.java
+++ lang/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         // Get the folder path as a CLI argument
-        String path = args[0];
+        // String path = args[0];
 
 //        LanguageModel model = new LanguageModel();
 //        model.addLanguage("english", path+"/lang-en");
